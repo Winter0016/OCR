a1ba9b034b41607084ae6a0969728a1084d3b1c7
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-06-03 13:07:50 +0700

    padding

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -59,7 +59,7 @@ function Login () {
 
 
     return (
-        <div className="min-h-screen bg-gray-700 py-6 flex flex-col justify-center sm:py-12">
+        <div className="min-h-screen bg-gray-700 flex flex-col justify-center pt-[10rem] pb-[10rem]">
             <div className="relative py-3 sm:max-w-xl sm:mx-auto">
                 <div
                     className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
