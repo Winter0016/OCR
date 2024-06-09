8fa47c9a18c0746b6c75cceeefb37b7da19a401e
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-06-09 16:01:45 +0700

    add individual delete function for each item in history

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -139,6 +139,36 @@ function History() {
     }
   };
 
+  const handledelete = async(date) =>{
+    const userEmail = auth.currentUser.email;
+    const docRef =doc(db,"history",userEmail);
+    try {
+      setdeleteprocess(true);
+      const docSnapshot = await getDoc(docRef);
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        const newData = { ...data };
+        Object.keys(newData).forEach((key) => {
+          if (newData[key].date == date) {
+            delete newData[key];
+          }
+        });
+        await setDoc(docRef, newData);
+        // console.log("Checked items deleted successfully.");
+        setcheckederror("");
+        setconfirm(false);
+      }
+    } catch (err) {
+      setdeleteprocess(false)
+      // console.error("Error deleting checked items:", err);
+      setcheckederror(err.message);
+    }finally{
+      await fetchUserData();
+      setdeleteprocess(false)
+    }
+
+  }
+
 
   useEffect(() => {
     if (!loading) {
@@ -154,11 +184,6 @@ function History() {
 
   };
 
-  // if(checkedItems){
-  //   console.log(JSON.stringify(checkedItems));
-  // }
-
-
   const handleMasterCheckboxChange = (isChecked) => {
     const newCheckedItems = {};
     for (let key in productlist) {
@@ -171,6 +196,9 @@ function History() {
     });
   };
 
+  const [confirm,setconfirm] = useState(false);
+  const [currentkey,setcurrentkey] = useState();
+
   return (
     <div className="pt-[16rem] p-[4rem] min-h-screen font-mono bg-gray-700">
       {array.length <= 0 ? (
@@ -178,10 +206,10 @@ function History() {
         ): (
           <>
             {array && (
-              <table className="min-w-full bg-gray-800 border-none rounded-3xl">
-                <thead>
-                  <div className="flex flex-col items-center w-full mt-5">
-                    <button className="mt-[4rem] p-4 rounded-xl text-white bg-red-900" disabled={deleteprocess} onClick={handleDeleteChecked}>{deleteprocess? "Deleting..." : "Delete Checked Items"}</button>
+              <div className="bg-gray-800 border-none rounded-3xl">
+                <div className="flex justify-center p-7 gap-6 flex-wrap items-center w-full mt-[1rem]">
+                  <div className="flex flex-col items-center">
+                    <button className="p-4 rounded-xl text-white bg-red-900" disabled={deleteprocess} onClick={handleDeleteChecked}>{deleteprocess? "Deleting..." : "Delete Checked Items"}</button>
                     {
                       checkederror ? (
                         <>
@@ -191,74 +219,94 @@ function History() {
                         <></>
                       )
                     }
-                    <div className="flex items-center mt-6 gap-3 text-white">
-                      <input className="size-8"
-                        type="checkbox"
-                        onChange={(e) => handleMasterCheckboxChange(e.target.checked)}
-                      />
-                      <label>Select All</label>
-                    </div>
                   </div>
-                  <tr>
-                    <th className="border-gray-300 p-9 text-blue-500 text-3xl">Time</th>
-                    <th className="border-gray-300 p-9 text-white text-3xl">OCR Picture</th>
-                    <th className="border-gray-300 p-9 text-green-500 text-3xl">OCR JSON</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {Object.keys(array).map((key) => (
-                    <tr key={array[key].date}>
-                      <td className="border-gray-300 p-7 text-blue-500 text-xl">
-                        <div className="flex items-center">
-                          <input className="mr-5 size-8" type="checkbox" onChange={(e) => handleCheckboxChange(array[key].date, e.target.checked)} />
-                          <span className="">{array[key].date}</span>
-                        </div>
-                      </td>
-                      <td className="border-gray-300 p-7 flex justify-center items-center">
-                        <img className="w-[20rem]" src={array[key].ocr_picture} alt="" />
-                      </td>
-                      <td className="border-gray-300 pt-7 pb-7">
-                        <div className="flex flex-col items-center">
-                          <pre className="text-green-500 text-wrap">
-                          {JSON.stringify(JSON.parse(array[key].ocr_json), null, 2)}
-                          </pre>
-                          {
-                              shareurl ? (
-                                  <>
-                                      <div className="flex gap-2 mt-[1rem] mb-2">
-                                          <h1 className="text-lg text-blue-400 border-[1px] text-center p-2 rounded-xl" >SHARE URL</h1>
-                                          <button className="text-lg border-[1px] p-2 rounded-xl" onClick={copyToClipboard}>COPY</button>
-                                      </div>
-                                      {copySuccess && <span style={{color: "green"}}>Copied!</span>}
-                                  </>
-                              ):(
-                                  <>
-                                      {
-                                          urlprocess ? (
-                                              <>
-                                                  <div className="mt-[1rem] mb-[1rem]">creating url....</div>
-                                              </>
-                                          ):(
-                                              <></>
-                                          )
-                                      }
-                                  </>
-                              )
-                          }
-                          <div className="flex gap-3 w-fit mt-2">
-                            <button onClick={() => handleShare(array[key].ocr_json)} className="cursor-pointer group relative flex gap-1.5 px-8 py-2 bg-black bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md mt-2">
-                              Share
-                            </button>  
-                            <button onClick={() => handleDownload(array[key].ocr_json)} className="cursor-pointer group relative flex gap-1.5 px-8 py-2 bg-black bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md mt-2">
-                              Download
-                            </button>  
-                          </div>
-                        </div>
-                      </td>        
+                  <div className="flex items-center gap-3 text-white">
+                    <input className="size-8"
+                      type="checkbox"
+                      onChange={(e) => handleMasterCheckboxChange(e.target.checked)}
+                    />
+                    <div className="">Select All</div>
+                  </div>
+                </div>
+                <table className="min-w-full">
+                  <thead>
+                    <tr>
+                      <th className="border-gray-300 p-9 text-blue-500 text-3xl">Time</th>
+                      <th className="border-gray-300 p-9 text-white text-3xl">OCR Picture</th>
+                      <th className="border-gray-300 p-9 text-green-500 text-3xl">OCR JSON</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
+                  </thead>
+                  <tbody>
+                    {Object.keys(array).map((key) => (
+                      <tr key={array[key].date}>
+                        <td className="border-gray-300 p-7 text-blue-500 text-xl">
+                          <div className="flex items-center">
+                            <input className="mr-5 size-8" type="checkbox" onChange={(e) => handleCheckboxChange(array[key].date, e.target.checked)} />
+                            <span className="">{array[key].date}</span>
+                          </div>
+                        </td>
+                        <td className="border-gray-300 p-7 flex justify-center items-center">
+                          <img className="w-[20rem]" src={array[key].ocr_picture} alt="" />
+                        </td>
+                        <td className="border-gray-300 pt-7 pb-7">
+                          <div className="flex flex-col items-center">
+                            <pre className="text-green-500 text-wrap">
+                            {JSON.stringify(JSON.parse(array[key].ocr_json), null, 2)}
+                            </pre>
+                            {
+                                shareurl ? (
+                                    <>
+                                        <div className="flex gap-2 mt-[1rem] mb-2">
+                                            <h1 className="text-lg text-blue-400 border-[1px] text-center p-2 rounded-xl" >SHARE URL</h1>
+                                            <button className="text-lg border-[1px] p-2 rounded-xl" onClick={copyToClipboard}>COPY</button>
+                                        </div>
+                                        {copySuccess && <span style={{color: "green"}}>Copied!</span>}
+                                    </>
+                                ):(
+                                    <>
+                                        {
+                                            urlprocess ? (
+                                                <>
+                                                    <div className="mt-[1rem] mb-[1rem]">creating url....</div>
+                                                </>
+                                            ):(
+                                                <></>
+                                            )
+                                        }
+                                    </>
+                                )
+                            }
+                            <div className="flex gap-3 w-fit h-fit mt-2">
+                              <button onClick={() => handleShare(array[key].ocr_json)} className=" w-fit h-fit cursor-pointer group relative flex gap-1.5 px-8 py-2 bg-black bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md mt-2">
+                                Share
+                              </button>  
+                              <button onClick={() => handleDownload(array[key].ocr_json)} className=" w-fit h-fit cursor-pointer group relative flex gap-1.5 px-8 py-2 bg-black bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md mt-2">
+                                Download
+                              </button>  
+                              <div className="flex flex-col gap-4">
+                                <button onClick={() => {setconfirm((prev) => !prev);setcurrentkey(key)}} className="cursor-pointer group relative flex gap-1.5 px-8 py-2 bg-red-700 hover:bg-red-600 bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md mt-2">
+                                  {confirm && key == currentkey ? "Cancel" : "Delete"}
+                                </button> 
+                                {
+                                  confirm && key == currentkey ? (
+                                    <>
+                                      <button disabled={deleteprocess} onClick={() => handledelete(array[key].date)} className="cursor-pointer group relative flex gap-1.5 px-8 py-2 bg-green-700 hover:bg-green-600 bg-opacity-80 text-[#f1f1f1] rounded-3xl hover:bg-opacity-70 transition font-semibold shadow-md mt-2">
+                                        {deleteprocess? "Deleting..." : "Confirm"}
+                                      </button>                     
+                                    </>
+                                  ) :(
+                                    <></>
+                                  )
+                                }
+                              </div> 
+                            </div>
+                          </div>
+                        </td>        
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             )}       
           </>
         )
