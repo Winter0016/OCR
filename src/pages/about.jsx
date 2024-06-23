b9e5edd5d696192305edf5a3b85aaed94788ae2d
Winter0016 <chauquangphuc2604.2604@gmail.com>
2024-06-23 10:07:24 +0700

    ád

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -59,27 +59,26 @@ function About() {
                         {
                           hovered2 && (
                             <>
-                              <div className=' absolute top-[4.5rem] left-5 z-10'>
+                              {/* <div className=' absolute top-[4.5rem] left-5 z-10'>
                                 <img src={img.left_horn} className=' size-[5rem]' alt="" />
                               </div>
                               <div className=' absolute top-[4.5rem] right-4 z-10'>
                                 <img src={img.right_horn} className=' size-[5rem]' alt="" />
-                              </div>
+                              </div> */}
                               <div className=' absolute top-0 transform rotate-180'>
                                 <img src={img.fire} className='' alt="" />
                               </div>
                               <div className=' absolute bottom-0'>
                                 <img src={img.fire} alt="" />
                               </div>
-                              <div className=' absolute bottom-3 size-[8rem]' id='siuu'>
+                              <div className=' absolute bottom-4 size-[10rem]' id='siuu'>
                                 <img src={img.siu} alt="" />
                               </div>
                             </>
                           )
                         }
                         <div className='text-xlc group-hover:italic group-hover:font-semibold'>Tuan Anh</div>
-                        <div className='text-xl opacity-0 -translate-x-14 group-hover:opacity-100 group-hover:translate-x-0 transform transition-all duration-500 text-center'>(AKA Ricon)</div>
-                        <div className='text-xl opacity-0 translate-y-12 group-hover:opacity-100 group-hover:translate-y-0 transform transition-all duration-500 text-center group-hover:text-orange-600 group-hover:font-semibold'>AI Analyst & Tester</div>
+                        <div className='text-xl opacity-0 translate-y-12 group-hover:opacity-100 group-hover:translate-y-0 transform transition-all duration-500 text-center group-hover:text-orange-600 group-hover:font-semibold'>AI Analyst & Tester</div><br></br>
                       </div>
 
                       <div className='group min-w-[15rem] h-[30rem] flex flex-col justify-center gap-3 items-center opacity-70 hover:bg-gray-600 hover:opacity-100 bg-gray-900'>
